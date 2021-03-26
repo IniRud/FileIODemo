8b1672ff449651c2645820278879d61b97e3ccd6
iniekan <iniakpan.e@gmail.com>
2021-03-25 19:47:40 -0600

    Add project files.

---
+++ FileIODemo/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo
+{
+    class Person
+    {
+        public Person(string firstName, string lastName, string uRL)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            URL = uRL;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string URL { get; set; }
+
+        public override string ToString()
+        {
+            return "fname " + FirstName + " lname " + LastName + " url " + URL;
+        }
+    }
+}
